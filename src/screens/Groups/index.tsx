0b267083593d8bc17727747994bb3edfc0c293c6
Carlos Alves <carlosaalves07@outlook.com>
2022-09-19 15:22:38 -0300

    feat: add assets image and path mapping

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+const Groups: React.FC = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default Groups;
