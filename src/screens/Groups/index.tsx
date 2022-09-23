e2e6229fb78be562e84d5b8e3bd8686eb41a8000
Carlos Alves <carlosaalves07@outlook.com>
2022-09-22 22:26:29 -0300

    feat: add new button component and listempty for group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,9 +5,11 @@ import * as S from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCArd } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <S.Container>
@@ -17,8 +19,13 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCArd title={item} onPress={() => {}} />}
+        renderItem={({ item }) => <GroupCArd title={item} />}
+        contentContainerStyle={groups.length == 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
+      <Button title="Criar nova turma" />
     </S.Container>
   );
 }
